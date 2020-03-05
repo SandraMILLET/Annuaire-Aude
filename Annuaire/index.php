d513d677384631df8a55ca65ebc00281fabdcc7c
axsomette <58254090+axsomette@users.noreply.github.com>
2020-03-05 16:26:12 +0100

    Add files via upload

---
+++ Annuaire/index.php
@@ -0,0 +1,77 @@
+
+<html><head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Title here</title>
+
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements. All other JS at the end of file. -->
+    <!--[if lt IE 9]>
+      <script src="js/html5shiv.js"></script>
+      <script src="js/respond.min.js"></script>
+    <![endif]-->
+
+    <!--headerIncludes-->
+
+	<!-- css files -->
+	<link rel="stylesheet" href="./css/style.css" type="text/css" media="all"> <!-- Style-CSS --> 
+	<link href="./css/font-awesome.min.css" rel="stylesheet" type="text/css" media="all">
+	<!-- //css files -->
+	
+	<!-- google fonts -->
+	<link href="//fonts.googleapis.com/css?family=Hind:300,400,500,600,700" rel="stylesheet">
+	<!-- //google fonts -->
+	
+<style type="text/css"></style></head>
+
+<body>
+
+    <div id="page" class="page">
+
+    <!-- main -->
+<div class="w3layouts-main bg bg1" title="image : 3000x800" style="background-color: rgb(0, 0, 0); margin-top: 0px; background-image: url(&quot;images/metiers.jpg&quot;);"> 
+	<div class="bg-layer cc_cursor">
+		<h1 class="editContent cc_pointer" title="Heading">Connexion</h1>
+		<div class="header-main editContent cc_pointer" title="Login Form" style="color: rgb(0, 0, 0); font-size: 16px; background-color: rgba(255, 255, 255, 0.04); font-family: Hind, sans-serif; outline: none; cursor: inherit;">
+			<div class="main-icon cc_pointer"> <img src="images/photo.png" width="60" height="90">
+			</div>
+			<div class="header-left-bottom">
+				<form action="#" method="post">
+					<div class="icon1 editContent cc_pointer">
+						<span class="fa fa-user" style="outline: none; cursor: inherit;"></span>
+						<input type="email" placeholder="Email Address" required="">
+					</div>
+					<div class="icon1 editContent cc_pointer">
+						<span class="fa fa-lock cc_pointer"></span>
+						<input type="password" placeholder="Password" required="">
+					</div>
+					<div class="login-check">
+						 <label class="checkbox editContent cc_pointer"><input type="checkbox" name="checkbox" checked=""><i> </i>Restez connecté&nbsp;</label>
+					</div>
+					<div class="bottom cc_pointer"><button class="btn editContent cc_pointer" style="outline: none; cursor: inherit; outline-offset: -2px;">connexion</button>&nbsp; &nbsp; &nbsp;&nbsp;</div>
+					<div class="links cc_cursor cc_pointer">
+						<p><a href="#" class="editContent cc_pointer" style="outline: rgba(233, 94, 94, 0.5) solid 2px; cursor: pointer; outline-offset: -2px;">Mot de passe oublié ?</a></p>
+						<p class="right cc_pointer"><a href="#" class="editContent" style="outline: none; cursor: inherit;">S'inscrire</a></p>
+						<div class="clear"></div>
+					</div>
+				</form>	
+			</div>
+			<div class="social-icons">
+				<ul>
+					<li class="editContent" style="outline: none; cursor: inherit;">ou connectez- vous avec :  <br> <img src="images/google.png" width="30px" height="30px"> </li>&nbsp;<br><br>
+				</ul>
+			</div>
+		</div>
+		
+		
+	</div>
+</div>	
+<!-- //main --></div>
+
+
+
+	
+<!-- RAJOUT DU FOOTER IMPORTANT  -->
+
+
+
+</body></html>
