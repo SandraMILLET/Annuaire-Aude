25f80a718d6fa1c778045ccff9c877bfe93db9d8
axsomette <58254090+axsomette@users.noreply.github.com>
2020-03-06 16:20:16 +0100

    Add files via upload
    LA PAGE D'INSCRIPTION TROP STYLÉE

---
+++ inscription/index.php
@@ -0,0 +1,116 @@
+
+<html><head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Title here</title>
+
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements. All other JS at the end of file. -->
+    <!--[if lt IE 9]>
+      <script src="js/html5shiv.js"></script>
+      <script src="js/respond.min.js"></script>
+    <![endif]-->
+
+    <!--headerIncludes-->
+
+	<!-- css files -->
+	<link rel="stylesheet" href="./css/style.css" type="text/css" media="all"> <!-- Style-CSS --> 
+	<link href="./css/font-awesome.min.css" rel="stylesheet" type="text/css" media="all">
+	<!-- //css files -->
+	
+	<!-- google fonts -->
+	<link href="//fonts.googleapis.com/css?family=Hind:300,400,500,600,700" rel="stylesheet">
+	<!-- //google fonts -->
+	
+<style type="text/css"></style></head>
+
+<body>
+
+    <div id="page" class="page">
+
+    <!-- main -->
+<div class="w3layouts-main bg bg1" title="image : 3000x800" style="background-color: rgb(0, 0, 0); margin-top: 0px; background-image: url(&quot;images/metiers.jpg&quot;);"> 
+	<div class="bg-layer cc_cursor">
+		<h1 class="editContent cc_pointer" title="Heading">Inscription</h1>
+		<div class="header-main editContent cc_pointer" title="Login Form" style="color: rgb(0, 0, 0); font-size: 16px; background-color: rgba(255, 255, 255, 0.04); font-family: Hind, sans-serif; outline: none; cursor: inherit;">
+			<div class="main-icon cc_pointer"> <p class="white"> Ajoutez une photo de profil <p>
+		</div>
+			
+			<div class="header-left-bottom">
+				<form action="#" method="post">
+				<div class="icon1 editContent cc_pointer">
+						<span class="fa fa-user" style="outline: none; cursor: inherit;"></span>
+						<input type="email" placeholder="Nom" required="">
+					</div>
+					<div class="icon1 editContent cc_pointer">
+						<span class="fa fa-user" style="outline: none; cursor: inherit;"></span>
+						<input type="email" placeholder="Prenom" required="">
+					</div>
+					<div class="icon1 editContent cc_pointer">
+						<span class="" style="outline: none; cursor: inherit;"> <img class="img" src="images/mail.svg">
+					</span>
+						<input type="email" placeholder="Entrez votre adresse email" required="">
+					</div>
+					<div class="icon1 editContent cc_pointer">
+						<span class="fa fa-lock cc_pointer"></span>
+						<input type="password" placeholder="mot de passe " required="">
+					</div>
+					<div class="icon1 editContent cc_pointer">
+						<span class="fa fa-lock cc_pointer"></span>
+						<input type="password" placeholder="confirmer le mot de passe " required="">
+					</div>
+					<div class="icon1 editContent cc_pointer">
+					<i class="fas fa-map-marker-alt"><img class="img" src="images/map.svg"></i>
+						<input type="email" placeholder="Siège social" required="">
+					</div>
+					<div class="icon1 editContent cc_pointer">
+					<i class="fas fa-map-marker-alt"><img class="img" src="images/siret.svg"></i>
+						<input type="email" placeholder="N° de Siret" required="">
+					</div>
+					<div class="icon1 editContent cc_pointer">
+					<i class="fas fa-map-marker-alt"><img class="img" src="images/image.svg"></i>
+						<input type="file" placeholder="N° de Siret" required="">
+					</div>
+					<div class="icon1 editContent cc_pointer">
+					<i class="fas fa-map-marker-alt"> Selectionnez votre métier </i>
+					<select class="Metier">
+                    <option id="Plomberie" value="Plombier">Plombier</option>
+                    <option id="Menuiserie" value="Menuisier">Menuisier</option>
+                    <option id="Electric" value="Electricien">Electricien</option>
+                    <option id="Peinture" value="Peintre">Peintre</option>
+					<option id="Estheticienne" value="Estheticienne">Estheticienne</option>
+				</select>
+				
+					
+					</div>
+					<div class="login-check">
+						
+					</div>
+					
+					<div id="column" class="links cc_cursor cc_pointer">
+					
+						<p class="right cc_pointer"><a href="#" class="editContent" style="outline: none; cursor: inherit;"></a> <button class="btn editContent cc_pointer" style="outline: none; cursor: inherit; outline-offset: -2px;">S'inscrire
+					</button></p>
+						<div class="clear"></div>
+					</div>
+				</form>	
+			</div>
+			<div class="social-icons">
+				<ul>
+				
+				</ul>
+			</div>
+		</div>
+		
+		
+	</div>
+</div>	
+<!-- //main --></div>
+
+
+
+	
+<!-- RAJOUT DU FOOTER IMPORTANT  -->
+
+
+
+</body></html>
