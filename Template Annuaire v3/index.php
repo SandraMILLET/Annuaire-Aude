12d491263b5b1261cbf8fd3085a4fecb7f400ca4
Kellian <contact@kellian-chamouard.fr>
2020-03-17 09:19:53 +0100

    Mise en ligne du template V3

---
+++ Template Annuaire v3/index.php
@@ -0,0 +1,182 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link
+            rel="stylesheet"
+            href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+            integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
+            crossorigin="anonymous">
+        <script
+            src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+            integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
+            crossorigin="anonymous"></script>
+        <script
+            src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+            integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
+            crossorigin="anonymous"></script>
+        <script
+            src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+            integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
+            crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="style.css">
+        <link
+            href="https://kit-free.fontawesome.com/releases/latest/css/free-v4-shims.min.css"
+            media="all"
+            rel="stylesheet">
+        <link
+            href="https://kit-free.fontawesome.com/releases/latest/css/free-v4-font-face.min.css"
+            media="all"
+            rel="stylesheet">
+        <link
+            href="https://kit-free.fontawesome.com/releases/latest/css/free.min.css"
+            media="all"
+            rel="stylesheet">
+        <title>Maquette Annuaire V3</title>
+    </head>
+    <body>
+
+        <div id="p1">
+
+            <div class="container-fluid">
+
+                <div class="row">
+
+                    <div class="col-sm-5 navga">
+
+                        ANNUAIRE DE L'AUDE
+
+                    </div>
+
+                    <div class="col-sm-4 navmi">
+
+                        <a href="#">Actus</a>
+
+                        <a href="#">Contact</a>
+
+                        <a href="#">Se Connecter</a>
+
+                        <a href="#">S'Inscrire</a>
+
+                        <a href="#">
+                            <i class="fas fa-search"></i>
+                        </a>
+
+                    </div>
+
+                    <div class="col-sm-3 navdr"></div>
+
+                </div>
+
+                <div class="col-sm-12 headmi">
+
+                    ANNUAIRE PROFESSIONNEL<br>
+                    Trouvez un artisan proche de chez vous !<br>
+                    <form class="headform">
+                        <div class="form-group">
+                            <select class="form-control">
+                                <option>Plombier</option>
+                                <option>Peintre</option>
+                                <option>Electricien</option>
+                                <option>Menuisier</option>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <select class="form-control">
+                                <option>Carcassonne</option>
+                                <option>Limoux</option>
+                                <option>Couiza</option>
+                                <option>Quillan</option>
+                                <option>Narbonne</option>
+                                <option>Lézignan</option>
+                                <option>Castelnaudary</option>
+                            </select>
+                        </div>
+                        <button type="button" class="btn btn-outline-light">Rechercher</button>
+                    </form>
+
+                </div>
+
+            </div>
+
+        </div>
+
+        <div id="p2">
+
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="card-deck">
+                        <div class="card">
+                            <img src="./img/cci1.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Batîment</h5>
+                                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                                    minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                                    commodo consequat.</p>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <img src="./img/cci2.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Esthétique</h5>
+                                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                                    minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                                    commodo consequat.</p>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <img src="./img/cci3.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Restauration</h5>
+                                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                                    minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                                    commodo consequat.</p>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <img src="./img/cci3.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Restauration</h5>
+                                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                                    minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                                    commodo consequat.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="centrerleboutton">
+                Trouvez tous les métiers dont vous avez besoin sur cet annuaire en ligne !<br><br>
+                <button type="button" class="btn btn-outline-light">Voir toutes les catégories de métiers</button>
+            </div>
+        </div>
+
+        <div id="p3">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-sm-6 actusl">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-sm-12 servtitle">Services</div>
+                                <div class="col-sm-12 servsubtitle">Lorem ipsum dolor sit amet, consectetur
+                                    adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+                                    aliqua.
+                                </div>
+                                <div class="col-sm-12 servimg">...</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 actusr">...</div>
+                </div>
+            </div>
+        </div>
+
+        <div id="p4"></div>
+
+    </body>
+</html>
